db7aac46a99df7e7c9fd7f60b3bcc99b93815073
André <andremessias.az@gmail.com>
2022-08-26 21:27:20 -0300

    setup: finish base folder structure and setup global styles

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,11 +1,21 @@
-import React from "react"
+import React from "react";
+
+import { About, Footer, Header, Skills, Testemonial, Work } from "./container";
+import {Navbar} from './components'
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testemonial />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
