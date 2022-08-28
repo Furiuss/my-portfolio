913c0cb1a22ba20b937e4eeed89dc3fc2c8539ae
André <andremessias.az@gmail.com>
2022-08-28 16:14:02 -0300

    fix: fix some bugs

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { HiMenuAlt4, HiX } from "react-icons/hi";
 import { motion } from "framer-motion";
 import "./Navbar.scss";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
 
   return (
     <motion.nav
       initial={{ y: '-100vw' }}
       animate={{ y: 0 }}
       transition={{type: 'spring', stiffness: 120}}
-      className="app__navbar"
+      className={scrolled ? 'scrolled app__navbar' : 'app__navbar'}
     >
       <div className="app__navbar-logo">
         <motion.h1
           whileHover={{
             scale: 1.1,
+            color: '#dd22cc',
             transition: {
               duration: 0.3,
               yoyo: Infinity,
