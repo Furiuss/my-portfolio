9ec0f911b918b273c7a19081495c082e694b123d
André <andremessias.az@gmail.com>
2022-08-31 15:59:51 -0300

    setup: setup wrapper high-order component

--- frontend_react/src/wrapper/AppWrap.js
+++ frontend_react/src/wrapper/AppWrap.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { NavigationDots, SocialMedia } from '../components';
 
-const AppWrap = () => (
-  <div>AppWrap</div>
-);
+const AppWrap = (Component, idName, className) => function HOC() {
+  return (
+    <div id={idName} className={`app__container ${className}`}>
+      <SocialMedia />
+
+      <div className="app__wrapper app__flex">
+        <Component />
+      </div>
+
+      <div className="copyright">
+        <p className="p-text">@2022 André</p>
+        <p className="p-text">All rights reserved</p>
+      </div>
+      <NavigationDots active={idName} />
+    </div>
+  );
+};
 
 export default AppWrap;
