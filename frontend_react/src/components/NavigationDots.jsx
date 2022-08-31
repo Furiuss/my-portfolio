05bdc093e1056c910354b245dfc84ef13d4732aa
André <andremessias.az@gmail.com>
2022-08-31 15:47:06 -0300

    setup/add: setup navigationDots component and add a new rule to eslint

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const NavigationDots = () => (
-  <div>NavigationDots</div>
+const NavigationDots = ({ active }) => (
+  <div className="app__navtigation">
+    {['home', 'about', 'work', 'skills', 'testemonial', 'contact'].map((item, index) => (
+      <a
+        href={item + index}
+        className="app__navigation-dot"
+        style={active === item ? { backgroundColor: '#9922ff' } : {}}
+      />
+    ))}
+  </div>
 );
 
 export default NavigationDots;
