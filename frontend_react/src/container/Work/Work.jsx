7b722ff7ee1b7c08fff859a46c425c14048db122
André <andremessias.az@gmail.com>
2022-09-01 15:26:38 -0300

    add: add appwrap to work section

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
+import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Work.scss';
 
@@ -107,4 +108,4 @@ const Work = () => {
   );
 };
 
-export default Work;
+export default AppWrap(Work, 'work');
